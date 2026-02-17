c621b9f341d74d4102a7b087ab72b6573ad8c4ef
samhart11037 <samantha.hart11037@gmail.com>
2026-02-16 23:50:39 -0500

    Update Second.js

--- Second.js
+++ Second.js
@@ -8,10 +8,8 @@ const runGame = function() {
     if (firstScore > secondScore){
         const gameResults = `${firstTeam} won the game, with a score of ${firstScore}!`;
     }
-    else{
-        if (secondScore > firstScore){
+    else if (secondScore > firstScore){
         const gameResults = `${secondTeam} won the game, with a score of ${secondScore}!`;
-        }
     }
     else{
         const gameResults = `The game was a tie, with both teams scoring ${firstScore} points!`;
