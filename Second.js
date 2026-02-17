077618d7ff68b82e2a7360e72f565a44c019822e
samhart11037 <samantha.hart11037@gmail.com>
2026-02-16 23:40:19 -0500

    Update Second.js

--- Second.js
+++ Second.js
@@ -1,8 +1,23 @@
 const runGame = function() {
     // Your code additions go here
+    const firstTeam = prompt("Enter the first team name: ");
+    const firstScore = prompt("Enter " + firstTeam + "'s score: ");
+    const secondTeam = prompt("Enter the second team name: ");
+    const secondScore = prompt("Enter " + secondTeam + "'s score: ");
 
+    if firstScore > secondScore{
+        const gameResults = `${firstTeam} won the game, with a score of ${firstScore}!`;
+    }
+    else{
+        if secondScore > firstScore{
+        const gameResults = `${secondTeam} won the game, with a score of ${secondScore}!`;
+        }
+    }
+    else{
+        const gameResults = `The game was a tie, with both teams scoring ${firstScore} points!`;
+    }
 
-  
+    document.getElementById("results").textContent = (gameResults);
   
     // Code that adds a new paragraph with a string to the "div" in the HTML file 
     const divId = document.getElementById("results");
