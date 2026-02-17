0e6dcdd8791ef2198cec371418d44df7e05534d9
samhart11037 <samantha.hart11037@gmail.com>
2026-02-16 23:56:34 -0500

    Update Second.js

--- Second.js
+++ Second.js
@@ -20,6 +20,6 @@ const runGame = function() {
     // Code that adds a new paragraph with a string to the "div" in the HTML file 
     const divId = document.getElementById("results");
     const newPara = document.createElement("p");
-    newPara.textContent = result;
+    newPara.textContent = gameResults;
     divId.appendChild(newPara);
 }
